727ba29c06121e3011eefbad326e8e16c52266d3
Buckwheat <Buckwheat@10.37.129.2>
2017-11-09 09:08:25 -0600

    The POS is now receiving the menu from the server. >FUCK YEAH<

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,15 +1,19 @@
 package server;
 
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.PriorityQueue;
 import java.util.Scanner;
-
+/*
 import allclasses.*;
 import allclasses.RestaurantItem;
 import allclasses.Order;
+*/
+
+import Food.*;
 
 public class Server
 {
